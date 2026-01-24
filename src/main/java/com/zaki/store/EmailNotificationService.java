47cdee0a57315a88a767856b823742c4ef93f99f
zakaryae bouabdallah <zakaprog@gmail.com>
2026-01-24 16:34:20 +0100

    using yaml instead of proprties

--- src/main/java/com/zaki/store/EmailNotificationService.java
+++ src/main/java/com/zaki/store/EmailNotificationService.java
@@ -1,10 +1,14 @@
 package com.zaki.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("email")
 public class EmailNotificationService implements NotificationService {
+  @Value("${stripe.timout}")
+  private int timout;
   public void sendMessage(){
     System.out.println("EMAIL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    System.out.println("timeout: "+timout);
   }
 }
